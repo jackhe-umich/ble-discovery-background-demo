33dd4afb23db6a79cc735dfa647cb9556fd77830
chshibo <chshibo@umich.edu>
2018-08-17 11:04:32 -0400

    Create new service on restart

--- app/src/main/java/org/thaliproject/nativetest/app/MainActivity.java
+++ app/src/main/java/org/thaliproject/nativetest/app/MainActivity.java
@@ -130,6 +130,8 @@ public class MainActivity extends AppCompatActivity implements
         //mConnectionEngine.start();
         //mConnectionEngine.makeDeviceDiscoverable();
         //mConnectionEngine.startBluetoothDeviceDiscovery();
+        Intent startServiceIntent = new Intent(this, BackgroundConnectionService.class);
+        startService(startServiceIntent);
     }
 
     @Override
